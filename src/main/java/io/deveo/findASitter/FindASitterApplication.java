8aeb39c89ec40f549a526d6a63bef7f03fae8fe4
Jason Kelly <jason-kelly@live.dk>
2018-05-01 13:23:19 +0200

    Initial commit

---
+++ src/main/java/io/deveo/findASitter/FindASitterApplication.java
@@ -0,0 +1,12 @@
+package io.deveo.findASitter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FindASitterApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FindASitterApplication.class, args);
+	}
+}
---
+++ src/test/java/io/deveo/findASitter/FindASitterApplicationTests.java
@@ -0,0 +1,16 @@
+package io.deveo.findASitter;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class FindASitterApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
